9e834e22494cebdeceab656ebcd96c96565eae3a
sebasRT <rtsebas11@gmail.com>
2024-08-14 19:17:24 -0400

    style: :wrench: Tailwind color added
    cerulean-blue added to colors palette

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,21 @@ export default {
 	darkMode: "selector",
 	theme: {
 		extend: {},
+		colors: {
+			'cerulean-blue': {
+				'50': '#eef6ff',
+				'100': '#d8eaff',
+				'200': '#badaff',
+				'300': '#8bc4ff',
+				'400': '#54a3ff',
+				'500': '#2d7eff',
+				'600': '#165cfa',
+				'700': '#0f46e6',
+				'800': '#1339ba',
+				'900': '#163592',
+				'950': '#122359',
+			}
+		}
 	},
 	plugins: [],
 }
