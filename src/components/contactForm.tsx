4e3a21f4a40fae644a424034d8de12df131fc8bd
sebasRT <rtsebas11@gmail.com>
2024-08-31 20:55:35 -0400

    refactor: Improve contact form submission flow

--- src/components/contactForm.tsx
+++ src/components/contactForm.tsx
@@ -1,19 +1,26 @@
 import { actions } from "astro:actions";
-import { useState, type FormEventHandler, type InputHTMLAttributes } from "react";
+import { useRef, useState, type FormEventHandler, type InputHTMLAttributes } from "react";
 
 const ContactForm = () => {
 
+    const [formState, setFormState] = useState<"filling" | "sending" | "sent" | "failed">("filling")
+    const form = useRef<HTMLFormElement>(null);
+
     const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
+        setFormState("sending");
         event.preventDefault();
         const formData = new FormData(event.currentTarget);
         const { error, data } = await actions.getVisitorMessage(formData);
-        
-        if (error) { alert(error.message); return }
-        alert(data.data);
-    };
+
+        if (error) { alert(error.message); setFormState("failed"); return }
+
+        form.current?.reset();
+        setFormState("sent");
+        setTimeout(() => setFormState("filling"), 5000); 
+      };
 
     return (
-        <form  onSubmit={handleSubmit} method="POST" className="flex flex-col items-center gap-5">
+        <form onSubmit={handleSubmit} method="POST" className="flex flex-col items-center gap-5" ref={form}>
             <div className="grid grid-cols-2 gap-5">
 
                 <Input name="Name" />
@@ -25,10 +32,20 @@ const ContactForm = () => {
                     onInput={(e) => e.currentTarget.setCustomValidity('')}
                 />
             </div>
-
-            <button type="submit" className="flex font-medium gap-5 items-center border border-primary-500 hover:bg-primary-500 hover:text-primary-50 px-4 rounded-full transition">
-                Send message
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" fillRule="evenodd" d="M3.291 3.309a.75.75 0 0 0-.976.996l3.093 6.945H13a.75.75 0 0 1 0 1.5H5.408l-3.093 6.945a.75.75 0 0 0 .976.996l19-8a.75.75 0 0 0 0-1.382z" clipRule="evenodd" /></svg>            </button>
+            {
+                (() => {
+                    switch (formState) {
+                        case "filling":
+                            return <button type="submit" className="flex font-medium gap-5 items-center border border-primary-500 hover:bg-primary-500 hover:text-primary-50 px-4 rounded-full transition">
+                                Send message
+                                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" fillRule="evenodd" d="M3.291 3.309a.75.75 0 0 0-.976.996l3.093 6.945H13a.75.75 0 0 1 0 1.5H5.408l-3.093 6.945a.75.75 0 0 0 .976.996l19-8a.75.75 0 0 0 0-1.382z" clipRule="evenodd" /></svg>            </button>
+                        case "sending":
+                            return <span className="">Sending your message <b>...</b></span>
+                        case "sent":
+                            return <span className=""> Thanks! I received your message ⚡</span>
+                        }
+                })()
+            }
         </form>
     )
 }
